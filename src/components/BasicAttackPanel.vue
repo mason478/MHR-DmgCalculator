c9267a8cba00c08b28354885f9bb028ade999536
mason478 <c029nkkd56@gmail.com>
2024-03-23 15:20:52 +0800

    use element-ui

--- src/components/BasicAttackPanel.vue
+++ src/components/BasicAttackPanel.vue
@@ -1,5 +1,12 @@
 <!-- Weapon basic attack values selection form-->
 <script setup lang="ts">
 import { ref } from 'vue'
+import { ElButton } from 'element-plus'
+import 'element-plus/dist/index.css'
 </script>
-<template><input type="number" id="weapon" /> 武器类型</template>
+<template>
+  <input type="number" id="weapon" /> 武器类型
+  <div>
+    <el-button>Hello Element</el-button>
+  </div>
+</template>
