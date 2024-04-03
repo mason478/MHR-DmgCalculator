d8df90a77e8a6db5a2632d734b211f1c01f077bb
mason478 <c029nkkd56@gmail.com>
2024-04-04 05:34:43 +0800

    style: rm useless codes

--- src/scripts/logic/CalculateDmg.ts
+++ src/scripts/logic/CalculateDmg.ts
@@ -124,6 +124,7 @@ class physicsDamageCalculator extends C {
   }
 
   /*
+
    * Calculate the physics attack power:
    * PhysicAttack = ((RawAttack * MultiCorrection1) + PlusCorrection) * MultiCorrection2
    *
@@ -187,19 +188,4 @@ function calcOtherElementCorrection(): number {
 
 function calcElementDamage(): number {
   todo
-}
-
-export default {
-  /**
-   * Calculate the total damage based on weapon attack power.
-   * @param weaponAttack Power of weapon's attack.
-   * @return Total damage of weapon.
-   */
-  calculateDmg(weaponAttack: number): number {
-    // Formula: TotalDamage = PhysicDamage + ElementDamage,
-    // PhysicDamage = MotionValue*(Attack/100)*(HitRate/100)*SharpnessCorrection*
-    let response: number = 0
-    response = weaponAttack * 2 // PhysicDamage
-    return response
-  }
 }
